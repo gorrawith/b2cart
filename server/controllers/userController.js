df23f08b51b69dd1ecb1f394f49fc96c1d98e278
b2time <gorrawith17@gmail.com>
2025-10-11 18:59:44 +0700

    lax

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -114,7 +114,7 @@ export const logout = async (req,res) =>{
         res.clearCookie('token',{
             httpOnly:true,
             secure:process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
             path: "/",
             
         });
