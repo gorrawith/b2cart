2df060401efec92aa32eb092d31fb238558b15b6
b2time <gorrawith17@gmail.com>
2025-10-11 18:48:14 +0700

    static

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -384,7 +384,7 @@ export const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
