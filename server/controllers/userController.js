6bed23ebcc30848198b6aec50325034d887b6246
b2time <gorrawith17@gmail.com>
2025-10-11 23:44:23 +0700

    userconytroller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -29,7 +29,8 @@ export const register = async (req,res)=>{
         res.cookie('token',token,{
             httpOnly: true, // Prevent JavaScript to access cookie
             secure : process.env.NODE_ENV === 'production', //Use secure cookies in production
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+            // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000, //Cookie expiration time
         })
         const mailOptions = {
@@ -82,7 +83,8 @@ export const  login = async (req,res) =>{
         res.cookie('token',token,{
             httpOnly: true, // Prevent JavaScript to access cookie
             secure : process.env.NODE_ENV === 'production', //Use secure cookies in production
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+            // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000, //Cookie expiration time
         })
         return res.json({success: true,user: 
@@ -114,7 +116,8 @@ export const logout = async (req,res) =>{
         res.clearCookie('token',{
             httpOnly:true,
             secure:process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === "production" ? "none" : 'strict',
+            // sameSite: process.env.NODE_ENV === "production" ? "none" : 'strict',
+            sameSite: "None",
             path: "/",
             
         });
@@ -385,7 +388,8 @@ export const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    //   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    sameSite: "None",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
